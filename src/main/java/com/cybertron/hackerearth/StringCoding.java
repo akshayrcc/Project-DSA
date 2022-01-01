5f3fc3d828c48a9fe851fdc8eae7646fb0fc250d
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-01 13:20:10 +0530

    StringCoding sol adding.

---
+++ src/main/java/com/cybertron/hackerearth/StringCoding.java
@@ -0,0 +1,47 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+/*
+ * uncomment this if you want to read input.
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+*/
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+class StringCoding {
+    public static void main(String args[] ) throws Exception {
+        /* Sample code to perform I/O:
+         * Use either of these methods for input
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();                // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();                 // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        */
+
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();                 // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+        // Write your code here
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // String name = br.readLine(); 
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<name.length;i++){
+            
+        // }
+    }
+}
