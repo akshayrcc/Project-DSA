ab557118d1f66ecdafc3f763dafe341a48c39d77
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-01 13:42:00 +0530

    StringCoding solution added.

--- src/main/java/com/cybertron/hackerearth/StringCoding.java
+++ src/main/java/com/cybertron/hackerearth/StringCoding.java
@@ -1,4 +1,4 @@
-/* IMPORTANT: Multiple classes and nested static classes are supported */
+package com.cybertron.hackerearth;/* IMPORTANT: Multiple classes and nested static classes are supported */
 
 /*
  * uncomment this if you want to read input.
@@ -12,8 +12,11 @@ import java.util.*;
 
 // Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class StringCoding {
-    public static void main(String args[] ) throws Exception {
+    public static void main(String[] args) throws Exception {
         /* Sample code to perform I/O:
          * Use either of these methods for input
 
@@ -29,19 +32,26 @@ class StringCoding {
 
         */
 
-
-        //Scanner
-        Scanner s = new Scanner(System.in);
-        String name = s.nextLine();                 // Reading input from STDIN
-        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-
         // Write your code here
 
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // String name = br.readLine(); 
-        // StringBuilder sb = new StringBuilder();
-        // for(int i=0;i<name.length;i++){
-            
-        // }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        char chr = inputString.charAt(0);
+        Long count = 1L;
+        for (int i = 1; i < inputString.length(); i++) {
+            char current = inputString.charAt(i);
+            if (current == chr) {
+                count++;
+            } else {
+                sb.append(chr);
+                sb.append(count);
+                chr = current;
+                count = 1L;
+            }
+        }
+        sb.append(chr);
+        sb.append(count);
+        System.out.println(sb);
     }
 }
