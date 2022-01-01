83b15d685e5153de281cb28a0bd4ff792018579d
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-01 19:32:25 +0530

    Roman to int solution added.

--- src/main/java/com/cybertron/hackerearth/StringCoding.java
+++ src/main/java/com/cybertron/hackerearth/StringCoding.java
@@ -14,6 +14,8 @@ import java.util.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 class StringCoding {
     public static void main(String[] args) throws Exception {
--- src/main/java/com/cybertron/leetcode/FizzBuzz.java
+++ src/main/java/com/cybertron/leetcode/FizzBuzz.java
@@ -1,22 +1,25 @@
 package com.cybertron.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
-	/*public static void main(String[] args) {
-		int number = 333;
-		for (int i = 1; i <= number; i++) {
-			if(i%15==0) {
-				System.out.println("FizzBuzz");
-			}else if(i%3==0) {
-				System.out.println("Fizz");
-			}else if(i%5==0) {
-				System.out.println("Buzz");
-			}else {
-				System.out.println(i);
-			}			
-		}
-	}*/
-	
-	/**
+    /*public static void main(String[] args) {
+        int number = 333;
+        for (int i = 1; i <= number; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }*/
+
+    /**
      * If number is divisible by 3, return "Fizz". If divisible by 5,
      * return "Buzz", and if divisible by both, return "FizzBuzz". Otherwise,
      * return the number itself.
@@ -24,22 +27,38 @@ public class FizzBuzz {
      * @Throws IllegalArgumentException for values < 1
      */
     public String fizzBuzz(int number) throws IllegalArgumentException {
-    	
-    	if(number <=0) {
-    		throw new IllegalArgumentException();
-    	}
-			if(number%15==0) {
-				//System.out.print("FizzBuzz");
-				return "FizzBuzz";
-			}else if(number%3==0) {
-				//System.out.print("Fizz");
-				return "Fizz";
-			}else if(number%5==0) {
-				//System.out.print("Buzz");
-				return "Buzz";
-			}else {
-				//System.out.print(i);
-				return String.valueOf(number);//Integer.toString(i);
-			}
+
+        if (number <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (number % 15 == 0) {
+            //System.out.print("FizzBuzz");
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            //System.out.print("Fizz");
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            //System.out.print("Buzz");
+            return "Buzz";
+        } else {
+            //System.out.print(i);
+            return String.valueOf(number);//Integer.toString(i);
+        }
+    }
+
+    public List<String> fizzBuzz_2(int n) {
+        List<String> lst = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                lst.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                lst.add("Fizz");
+            } else if (i % 5 == 0) {
+                lst.add("Buzz");
+            } else {
+                lst.add(String.valueOf(i));
+            }
+        }
+        return lst;
     }
 }
