6449f20100a83c00acf987006b12b8dbadb565a3
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-09 23:32:59 +0530

    Single num added.

---
+++ src/main/java/com/cybertron/leetcode/SingleNumber.java
@@ -0,0 +1,28 @@
+package com.cybertron.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNumber {
+    public int singleNumber_1(int[] nums) {
+        int singleNum = 0;
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i : nums) {
+            if (hashSet.contains(i)) {
+                hashSet.remove(i);
+            } else {
+                hashSet.add(i);
+            }
+        }
+        singleNum = hashSet.iterator().next();
+        return singleNum;
+    }
+
+    public int singleNumber_2(int[] nums) {
+        int n = 0;
+        for(int i : nums){
+            n ^= i;
+        }
+        return n;
+    }
+}
