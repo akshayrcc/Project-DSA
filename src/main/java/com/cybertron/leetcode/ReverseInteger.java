77ebf96db27478cea22c302ee2be38c78e646500
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-14 15:03:49 +0530

    Reverse int sol added.

--- src/main/java/com/cybertron/leetcode/ReverseInteger.java
+++ src/main/java/com/cybertron/leetcode/ReverseInteger.java
@@ -1,7 +1,7 @@
 package com.cybertron.leetcode;
 
 public class ReverseInteger {
-    public int reverse(int x) {
+    public int reverse_1(int x) {
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
@@ -11,5 +11,34 @@ public class ReverseInteger {
             rev = rev * 10 + pop;
         }
         return rev;
+    }
+
+    public int reverse_2(int x) {
+        boolean negative = x < 0;
+        if (negative) {
+            x *= -1;
+        }
+        int r = 0;
+        while (x != 0) {
+            int mod = x % 10;
+            if (r > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            r = (r * 10) + mod;
+            x = x / 10;
+            if (r < 0) {
+                return 0;
+
+            }
+        }
+        System.out.println(r);
+
+        if (negative) {
+            r *= -1;
+            if (r > 0) {
+                return 0;
+            }
+        }
+        return r;
     }
 }
