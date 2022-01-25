ecd97fc76e0f675db02f230bf25e5f5ca5d56c1c
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-25 22:00:08 +0530

    MaximumDepthOfBinaryTree added.

---
+++ src/main/java/com/cybertron/algorithms/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,13 @@
+package com.cybertron.algorithms.tree;
+
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        int bigger = Math.max(leftDepth, rightDepth);
+        return bigger + 1;
+    }
+}
