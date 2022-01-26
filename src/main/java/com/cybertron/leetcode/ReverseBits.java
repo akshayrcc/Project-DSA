fdc98a0f29b1588c72e9b0ee65d09616a57dd9a9
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-01-27 00:45:49 +0530

    ReverseBits!

---
+++ src/main/java/com/cybertron/leetcode/ReverseBits.java
@@ -0,0 +1,13 @@
+package com.cybertron.leetcode;
+
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res = res | (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
