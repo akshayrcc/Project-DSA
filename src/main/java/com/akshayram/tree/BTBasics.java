32eb98322eee53b851465a6155ac1c458dd04737
akshay-m2 <er.akshayrc@gmail.com>
2024-02-26 23:39:43 -0800

    same same but different... :P

--- src/main/java/com/akshayram/tree/BTBasics.java
+++ src/main/java/com/akshayram/tree/BTBasics.java
@@ -2,33 +2,41 @@ package com.akshayram.tree;
 
 public class BTBasics {
 
-  private int getTreeHeight(TreeNode node) {
-    if (node == null) return 0;
-    int leftHeight = getTreeHeight(node.left);
-    int rightHeight = getTreeHeight(node.right);
-    return 1 + Math.max(leftHeight, rightHeight);
-  }
-
-  private int distanceFromRoot(TreeNode node, int target) {
-    if (node == null) {
-      return -1; // Node not found
+    private int getTreeHeight(TreeNode node) {
+        if (node == null) return 0;
+        int leftHeight = getTreeHeight(node.left);
+        int rightHeight = getTreeHeight(node.right);
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 
-    if (node.val == target) {
-      return 0; // Node found at the root
+    private int distanceFromRoot(TreeNode node, int target) {
+        if (node == null) {
+            return -1; // Node not found
+        }
+
+        if (node.val == target) {
+            return 0; // Node found at the root
+        }
+
+        // Recursively search in left and right subtrees
+        int leftDistance = distanceFromRoot(node.left, target);
+        if (leftDistance != -1) {
+            return leftDistance + 1; // Found in left subtree
+        }
+
+        int rightDistance = distanceFromRoot(node.right, target);
+        if (rightDistance != -1) {
+            return rightDistance + 1; // Found in right subtree
+        }
+
+        return -1; // Not found in the tree
     }
 
-    // Recursively search in left and right subtrees
-    int leftDistance = distanceFromRoot(node.left, target);
-    if (leftDistance != -1) {
-      return leftDistance + 1; // Found in left subtree
+    //TC: O(n) SC: O(h)
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
     }
-
-    int rightDistance = distanceFromRoot(node.right, target);
-    if (rightDistance != -1) {
-      return rightDistance + 1; // Found in right subtree
-    }
-
-    return -1; // Not found in the tree
-  }
 }
