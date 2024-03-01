201d66e55c7dbfe25d352e462e00870903192763
akshay-m2 <er.akshayrc@gmail.com>
2024-02-29 23:42:11 -0800

    added isEvenOdd

--- src/main/java/com/akshayram/tree/BTBasics.java
+++ src/main/java/com/akshayram/tree/BTBasics.java
@@ -1,5 +1,8 @@
 package com.akshayram.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BTBasics {
 
     private int getTreeHeight(TreeNode node) {
@@ -57,5 +60,44 @@ public class BTBasics {
         int right = helper(root.right);
         diameter = Math.max(diameter, left + right);
         return Math.max(left, right) + 1;
+    }
+
+    private List<Integer> prev = new ArrayList<>();
+
+    public boolean isEvenOddTree(TreeNode root) {
+        TreeNode current = root;
+        return dfs(current, 0);
+    }
+
+    private boolean dfs(TreeNode current, int level) {
+        // Base case, an empty tree is Even-Odd
+        if (current == null) {
+            return true;
+        }
+
+        // Compare the parity of current and level
+        if (current.val % 2 == level % 2) {
+            return false;
+        }
+
+        // Add a new level to prev if we've reached a new level
+        while (prev.size() <= level) {
+            prev.add(0);
+        }
+
+        // If there are previous nodes on this level, check increasing/decreasing
+        // If on an even level, check that current's value is greater than the previous on this level
+        // If on an odd level, check that current's value is less than the previous on this level
+        if (prev.get(level) != 0 &&
+                ((level % 2 == 0 && current.val <= prev.get(level)) ||
+                        (level % 2 == 1 && current.val >= prev.get(level)))) {
+            return false;
+        }
+
+        // Add current value to prev at index level
+        prev.set(level, current.val);
+
+        // Recursively call DFS on the left and right children
+        return dfs(current.left, level + 1) && dfs(current.right, level + 1);
     }
 }
