ddeb661c5ff078a0c71c16e7948c5b6204fabda0
akshay-m2 <er.akshayrc@gmail.com>
2024-03-27 00:04:44 -0700

    subarray prod less than k

--- src/main/java/com/akshayram/basicprograms/ArraysMediums.java
+++ src/main/java/com/akshayram/basicprograms/ArraysMediums.java
@@ -79,5 +79,20 @@ public class ArraysMediums {
         nums[y] = temp;
     }
 
+    //713. Subarray Product Less Than K
+    //TC: O(n) SC: O(1)
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int left = 0, right = 0, product = 1, count = 0;
+        int N = nums.length;
+        while (right < N) {
+            product *= nums[right]; //expanding from right
+            while (product >= k) product /= nums[left++]; //shriking from left
+            count += 1 + (right - left); //adding all subarrays from the active window
+            right++;
+        }
+        return count;
+    }
+
 
 }
