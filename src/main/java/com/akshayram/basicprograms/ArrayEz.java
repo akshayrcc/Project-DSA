5e325ce92a62f40ea01a83d6770da84ecfd064e6
akshay-m2 <er.akshayrc@gmail.com>
2024-06-12 23:58:37 -0700

    sorted colors in place

--- src/main/java/com/akshayram/basicprograms/ArrayEz.java
+++ src/main/java/com/akshayram/basicprograms/ArrayEz.java
@@ -131,6 +131,29 @@ class ArrayEz {
         return true;
     }
 
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+        int zero = 0, one = 0, two = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == 0) {
+                zero++;
+            } else if (nums[i] == 1) {
+                one++;
+            } else {
+                two++;
+            }
+        }
+        for (int i = 0; i < zero; i++) {
+            nums[i] = 0;
+        }
+        for (int i = zero; i < zero + one; i++) {
+            nums[i] = 1;
+        }
+        for (int i = zero + one; i < len; i++) {
+            nums[i] = 2;
+        }
+    }
+
     public int countCharacters(String[] words, String chars) {
         int sum = 0;
         // create a freqMap for chars string
